06c34021b22e61efc9e4736cca7bfaad94e8393a
Raj Desai <raj7desai@gmail.com>
2020-05-10 14:08:15 +0530

    Fix bug where neames were repeated in columns

--- housie.js
+++ housie.js
@@ -95,143 +95,153 @@ let random = (min, max) => { // min and max included
 
 let addToRow3 = () => {
 
-  let count = 0;
-  for (let i = 0; i < ticket[2].length; ++i) {
-    if (ticket[2][i] == 0)
-      count++;
-  }
-  console.log("on line 133 " + count)
+    let count = 0;
+    for (let i = 0; i < ticket[2].length; ++i) {
+      if (ticket[2][i] == 0)
+        count++;
+    }
+    console.log("on line 133 " + count)
 
-  while (count > 4) {
-    let n = random(0, 8)
-    let m = random(0, 9)
+    while (count > 4) {
+      let n = random(0, 8)
+      let m = random(0, 9)
 
-    celebNumber = n * 10 + m
-    console.log(n + " " + m + " " + celebNumber)
-    console.log(celebs[celebNumber - 1])
+      celebNumber = n * 10 + m
+      console.log(n + " " + m + " " + celebNumber)
+      console.log(celebs[celebNumber])
 
 
-    if (ticket[2][n] == 0) {
-      ticket[2][n] = celebs[celebNumber - 1]
-      count = count - 1
-    } else {
-      console.log("false")
+      if (ticket[2][n] == 0) {
+        if (celebs[celebNumber] == ticket[1][n] || celebs[celebNumber] == ticket[0][n]) {
+          console.log("Same celeb")
+        } else {
+          ticket[2][n] = celebs[celebNumber]
+          count = count - 1
+        }
+      } else {
+        console.log("false")
+      }
     }
   }
-}
-
-let addToRow2 = () => {
-
-  let count = 0;
-  for (let i = 0; i < ticket[2].length; ++i) {
-    if (ticket[2][i] == 0)
-      count++;
-  }
-  console.log("on line 133 " + count)
-
-  while (count > 4) {
-    let n = random(0, 8)
-    let m = random(0, 9)
-
-    celebNumber = n * 10 + m
-    console.log(n + " " + m + " " + celebNumber)
-    console.log(celebs[celebNumber - 1])
 
 
-    if (ticket[1][n] == 0) {
-      ticket[1][n] = celebs[celebNumber - 1]
-      count = count - 1
-    } else {
-      console.log("false")
-    }
-  }
-}
+      let addToRow2 = () => {
 
-let addToRow1 = () => {
+        let count = 0;
+        for (let i = 0; i < ticket[2].length; ++i) {
+          if (ticket[2][i] == 0)
+            count++;
+        }
+        console.log("on line 133 " + count)
 
-  let count = 0;
-  for (let i = 0; i < ticket[2].length; ++i) {
-    if (ticket[2][i] == 0)
-      count++;
-  }
-  console.log("on line 133 " + count)
+        while (count > 4) {
+          let n = random(0, 8) 
+          let m = random(0, 9)
 
-  while (count > 4) {
-    let n = random(0, 8)
-    let m = random(0, 9)
-
-    celebNumber = n * 10 + m
-    console.log(n + " " + m + " " + celebNumber)
-    console.log(celebs[celebNumber - 1])
+          celebNumber = n * 10 + m
+          console.log(n + " " + m + " " + celebNumber)
+          console.log(celebs[celebNumber])
 
 
-    if (ticket[0][n] == 0) {
-      ticket[0][n] = celebs[celebNumber - 1]
-      count = count - 1
-    } else {
-      console.log("false")
-    }
-  }
-}
+          if (ticket[1][n] == 0) {
+            if (celebs[celebNumber] == ticket[2][n] || celebs[celebNumber] == ticket[0][n]) {
+              console.log("Same celeb")
+            } else {
+              ticket[1][n] = celebs[celebNumber]
+              count = count - 1
+            }
+          
+          } else {
+            console.log("false")
+          }
+        }
+      }
+
+      let addToRow1 = () => {
+
+        let count = 0;
+        for (let i = 0; i < ticket[2].length; ++i) {
+          if (ticket[2][i] == 0)
+            count++;
+        }
+        console.log("on line 133 " + count)
+
+        while (count > 4) {
+          let n = random(0, 8)
+          let m = random(0, 9)
+
+          celebNumber = n * 10 + m
+          console.log(n + " " + m + " " + celebNumber)
+          console.log(celebs[celebNumber - 1])
 
 
-let makeTicket = () => {
-
-  addToRow1()
-  addToRow2()
-  addToRow3()
-  console.log(ticket)
-}
-
-/* console.log(ticket)
- */
+          if (ticket[0][n] == 0) {
+            ticket[0][n] = celebs[celebNumber]
+            count = count - 1
+          } else {
+            console.log("false")
+          }
+        }
+      }
 
 
+      let makeTicket = () => {
 
+        addToRow1()
+        addToRow2()
+        addToRow3()
+        console.log(ticket)
+      }
 
-
-
-let createTicket = (array) => {
-
-  let table = document.createElement('table');
-  let row1 = document.createElement('row');
-  let row2 = document.createElement('row');
-  let row3 = document.createElement('row');
+      /* console.log(ticket)
+       */
 
 
 
-  for (let x = 0; x < array[0].length; x++) {
 
-    let td = document.createElement('td');
-    td.innerHTML = array[0][x];
-    row1.appendChild(td);
-  }
 
-  table.appendChild(row1);
 
-  for (let x = 0; x < array[1].length; x++) {
+      let createTicket = (array) => {
 
-    let td = document.createElement('td');
-    td.innerHTML = array[1][x];
-    row2.appendChild(td);
-  }
+        let table = document.createElement('table');
+        let row1 = document.createElement('row');
+        let row2 = document.createElement('row');
+        let row3 = document.createElement('row');
 
-  table.appendChild(row2);
 
-  for (let x = 0; x < array[2].length; x++) {
 
-    let td = document.createElement('td');
-    td.innerHTML = array[2][x];
-    row3.appendChild(td);
-  }
+        for (let x = 0; x < array[0].length; x++) {
 
-  table.appendChild(row3);
-  return table;
+          let td = document.createElement('td');
+          td.innerHTML = array[0][x];
+          row1.appendChild(td);
+        }
 
-}
+        table.appendChild(row1);
 
-window.onload = function () {
-  makeTicket()
-  let table = createTicket(ticket)
-  document.body.appendChild(table);
-}
+        for (let x = 0; x < array[1].length; x++) {
+
+          let td = document.createElement('td');
+          td.innerHTML = array[1][x];
+          row2.appendChild(td);
+        }
+
+        table.appendChild(row2);
+
+        for (let x = 0; x < array[2].length; x++) {
+
+          let td = document.createElement('td');
+          td.innerHTML = array[2][x];
+          row3.appendChild(td);
+        }
+
+        table.appendChild(row3);
+        return table;
+
+      }
+
+      window.onload = function () {
+        makeTicket()
+        let table = createTicket(ticket)
+        document.getElementById('ticket').appendChild(table);
+      }
